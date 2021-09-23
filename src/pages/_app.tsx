d8eba543f72f37ed7a6542c5f66088977353e0d4
shoma07 <23730734+shoma07@users.noreply.github.com>
2021-09-23 20:19:30 +0900

    add lint and fmt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "../../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+/* eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types */
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  /* eslint-disable-next-line react/jsx-props-no-spreading */
+  <Component {...pageProps} />
+);
+
+export default MyApp;
